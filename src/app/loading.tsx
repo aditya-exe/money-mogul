9e35726a11c93a5cde49077ee7d46a92e29dc05f
aditya <pokemaniacaditya@gmail.com>
2024-04-11 13:56:04 +0530

    FIX:loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
       <h1 className="mt-24 text-9xl font-extrabold italic tracking-tighter">
         Money Mogul
       </h1>
-      <Icons.loading className="h-8 w-8 animate-spin text-lime-500" />
+      <Icons.loading className="mt-32 h-8 w-8 animate-spin text-lime-500" />
     </div>
   );
 };
